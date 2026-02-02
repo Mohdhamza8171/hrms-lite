6da9cd7b938a18d397e91c33de8a6f0bea013757
M0hdhamza <hamza@trustsignal.io>
2026-02-02 15:40:19 +0530

    allowed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,7 +35,7 @@ JWT_SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
